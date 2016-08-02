1a28eba19708cb64784ba99b36f46a37fa8afca8
leejeongwha <ljk2491@nhn.com>
2016-08-02 18:37:20 +0900

    bridge 패턴 추가

--- src/com/naver/pattern/bridge/Main.java
+++ src/com/naver/pattern/bridge/Main.java
@@ -2,6 +2,11 @@ package com.naver.pattern.bridge;
 
 public class Main {
 
+	/**
+	 * 추상적 개념인 PrintMorseCode와 구체적 구현인 MorseCodeFunction을 분리
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		PrintMorseCode code = new PrintMorseCode(new SoundMCF());
 		code.g().a().r().a().m();

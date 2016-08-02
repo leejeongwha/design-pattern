1d49d6436a2c130b96efcba7b057b6f335466341
leejeongwha <ljk2491@nhn.com>
2016-08-02 19:02:38 +0900

    visitor 패턴 설명 추가

--- src/com/naver/pattern/visitor/TotalHomeTask.java
+++ src/com/naver/pattern/visitor/TotalHomeTask.java
@@ -12,6 +12,12 @@ public class TotalHomeTask implements HomeTaskElement {
 		homeTaskElements.add(new BabyCareTask());
 	}
 
+	/*
+	 * 각 Task별로 파라미터로 넘겨받은 Visitor의 visit메서드를 호출
+	 * 
+	 * @see com.naver.pattern.visitor.HomeTaskElement#accept(com.naver.pattern.
+	 * visitor.HomeVisitor)
+	 */
 	@Override
 	public void accept(HomeVisitor homeVisitor) {
 		Iterator it = homeTaskElements.iterator();

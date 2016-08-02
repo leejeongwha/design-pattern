30bd44dc73f6dd044c4a175c7f49a66245b7ffdf
leejeongwha <ljk2491@nhn.com>
2016-08-02 19:08:19 +0900

    facade 패턴 설명 추가

--- src/com/naver/pattern/facade/Main.java
+++ src/com/naver/pattern/facade/Main.java
@@ -3,6 +3,11 @@ package com.naver.pattern.facade;
 import com.naver.pattern.facade.system.Computer;
 
 public class Main {
+	/**
+	 * 인터페이스를 단순화!!
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Computer facade = new Computer();
 		facade.startComputer();
